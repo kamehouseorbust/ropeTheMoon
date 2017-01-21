4a7204104ce4ff1fdf54ee8f766819032eff1885
kamehouseorbust <haydendrussell@gmail.com>
2017-01-21 04:04:05 -0500

    Final Commit For Night
    Got all things spawning. Delete on pass player. Position on spawn is
    set to random y-position for both objects. Both objects create unique
    particle effects when destroyed.

---
+++ Assets/Hayden's Stuff/rockSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rockSpawn : MonoBehaviour {
+
+	public GameObject rockPrefab;
+	public float timerLength = 2f;
+	private float currentTime = 0;
+	private Transform rockSpawnTransform;
+
+	// Use this for initialization
+	void Start () {
+		rockSpawnTransform = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentTime > timerLength) {
+			int spawnY = Random.Range(-1,-4);
+			rockSpawnTransform.position = new Vector3 (11.1f,spawnY);
+			GameObject.Instantiate (rockPrefab,transform.position, transform.rotation);
+			currentTime = 0;
+		}
+		currentTime += Time.deltaTime;	
+	}
+}
---
+++ Assets/Hayden's Stuff/starBe.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starBe : MonoBehaviour {
+
+	private Transform starTransform;
+	public float starSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		starTransform = GetComponent<Transform> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		starTransform.Translate (-starSpeed*Time.deltaTime, 0, 0);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Player") {
+			Debug.Log ("Contact!");
+			Destroy (this.gameObject);
+		}
+		if (coll.gameObject.tag == "killable") {
+			Debug.Log ("Kill Contact!");
+			Destroy (this.gameObject);
+		}
+	}
+
+}
