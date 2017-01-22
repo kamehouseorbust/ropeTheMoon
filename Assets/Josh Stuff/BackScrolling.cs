7c3c47612a5d8d9638019ed4148e9fde955027e0
JoshM <josh.moore@gatech.edu>
2017-01-21 22:42:38 -0500

    Updated and Synced between Hayden and Me 1042pm

---
+++ Assets/Josh Stuff/BackScrolling.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackScrolling : MonoBehaviour {
+
+    public float scrollSpeed;
+    private Material m;
+
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer rend = this.GetComponent<SpriteRenderer>();
+        m = rend.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        m.mainTextureOffset = new Vector2(x, 0);
+	}
+}
---
+++ Assets/Josh Stuff/TestMoveDisplace.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMoveDisplace : MonoBehaviour {
+
+    private Transform t;
+    public float moveSpeed = .1f;
+
+	// Use this for initialization
+	void Start () {
+        t = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float vert = Input.GetAxis("Vertical");
+        t.Translate(0, vert * moveSpeed * Time.deltaTime, 0);
+	}
+}
